b97b9d0d2524fcaae0bfe36fd19b27c9688958de
keyurvisavadiya <keyurvisavadiya124@gmail.com>
2026-03-09 15:18:01 +0530

    dynamic home screen and messages added to job details screen

--- lib/market/screen/job_detailed.dart
+++ lib/market/screen/job_detailed.dart
@@ -251,6 +251,7 @@ class JobDetailScreen extends StatelessWidget {
                             ),
                           ],
                         ),
+
                       ],
                     ),
                   ),
